18d778387d7883d298cb00dd41e9dc8eaf3a7950
Harsh Prasad <explorerharsh@gmail.com>
2021-07-02 14:36:00 +0530

    Stripe settings completed

--- stripebackend/index.js
+++ stripebackend/index.js
@@ -14,6 +14,39 @@ app.get("/", (req, res) => {
   res.send("It works!");
 });
 
+app.post("/payment", (req, res) => {
+  const { product, token } = req.body;
+  console.log("PRODUCT ", product);
+  console.log("PRICE ", product.price);
+  const idempotencyKey = uuidv4();
+
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create(
+        {
+          amount: product.price * 100,
+          currency: "usd",
+          customer: customer.id,
+          receipt_email: token.email,
+          description: `Your purchase of ${product.name}`,
+          shipping: {
+            name: token.card.name,
+            address: {
+              country: token.card.address_country,
+            },
+          },
+        },
+        { idempotencyKey }
+      );
+    })
+    .then((result) => res.status(200).json(result))
+    .catch((err) => console.log(err));
+});
+
 // listen
 app.listen(8282, () => {
   console.log("Listening at port 8282");
