0cd082302cf458065d2ddb499842bd25c37c88cc
Harsh Prasad <explorerharsh@gmail.com>
2021-07-02 15:01:44 +0530

    Adding buy now button

--- stripefrontend/src/App.js
+++ stripefrontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import StripeCheckout from "react-stripe-checkout";
 
 function App() {
   const [product, setProduct] = useState({
@@ -17,6 +18,9 @@ function App() {
         <a className="App-link" href="#" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <StripeCheckout stripeKey="" token="" name="Buy React">
+          <button className="btn-large green">Buy Now</button>
+        </StripeCheckout>
       </header>
     </div>
   );
