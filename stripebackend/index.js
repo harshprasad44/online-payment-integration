63506e41ff17db71360ace24c64f82174c0bb2c5
Harsh Prasad <explorerharsh@gmail.com>
2021-07-02 14:10:33 +0530

    Listening at port 8282

--- stripebackend/index.js
+++ stripebackend/index.js
@@ -1,6 +1,20 @@
 const cors = require("cors");
 const express = require("express");
-// const stripe = require("stripe")("")
-const uuid = require("uuid/v4");
-
+const stripe = require("stripe")("pk_test_51J8hq2SFKDXtErfjmVAfvwIv4esMWrTQQCztHec2mwn6dhWdSLhq2K9wMap5zaRZtEncEELHUYeiNn7wa0lRdqZq00mGaZt4BE");
+const { v4: uuidv4 } = require("uuid");
+uuidv4();
 const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("It works!");
+});
+
+// listen
+app.listen(8282, () => {
+  console.log("Listening at port 8282");
+});
