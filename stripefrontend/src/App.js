90d0443373099e7931c7a9e3d993f9fc0c79b76b
Harsh Prasad <explorerharsh@gmail.com>
2021-07-02 16:24:46 +0530

    Adding success after payment success

--- stripefrontend/src/App.js
+++ stripefrontend/src/App.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import StripeCheckout from "react-stripe-checkout";
 
-function App() {
+function App({ history }) {
   const [product, setProduct] = useState({
     name: "React from FB",
     price: 10,
     productBy: "facebook",
   });
+
+  const [success, setSucceess] = useState(false);
+
+  // useEffect(() => {
+  //   if (success) {
+  //     <Redirect to="/success" />;
+  //   }
+  // }, [success]);
 
   const makePayment = (token) => {
     const body = {
@@ -28,6 +37,7 @@ function App() {
         console.log("RESPONSE ", response);
         const { status } = response;
         console.log("STATUS ", status);
+        setSucceess(true);
       })
       .catch((error) => console.log(error));
   };
@@ -37,12 +47,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <a className="App-link" href="#" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
         <StripeCheckout stripeKey="pk_test_51J8hq2SFKDXtErfjmVAfvwIv4esMWrTQQCztHec2mwn6dhWdSLhq2K9wMap5zaRZtEncEELHUYeiNn7wa0lRdqZq00mGaZt4BE" token={makePayment} name="Buy React" amount={product.price * 100} shippingAddress billingAddress>
           <button className="btn-large green">Buy Now</button>
         </StripeCheckout>
+
+        {success && <a href="/success">Success</a>}
       </header>
     </div>
   );
