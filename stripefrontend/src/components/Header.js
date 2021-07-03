608944b29d60189d69389edd0ebfacad7c34495b
Harsh Prasad <explorerharsh@gmail.com>
2021-07-03 13:19:53 +0530

    Header file completed

--- stripefrontend/src/components/Header.js
+++ stripefrontend/src/components/Header.js
@@ -7,29 +7,12 @@ const Header = () => {
   return (
     <header>
       <Navbar style={{ height: "90px" }} bg="dark" variant="dark" expand="lg" collapseOnSelect>
-        <Container>
+        <Container style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
           <LinkContainer to="/">
             <Navbar.Brand>
               <img src="https://i.ibb.co/yRj7Bvh/new-logo-01.png" width="100px" alt="Logo" />
             </Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            {/* <Route render={({ history }) => <SearchBox history={history} />} /> */}
-            <Nav className="ml-auto">
-              <LinkContainer to="/cart">
-                <Nav.Link>
-                  <i style={{ color: "black" }} className="fas fa-shopping-cart"></i> Cart
-                </Nav.Link>
-              </LinkContainer>
-
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <i className="fas fa-user"></i> Sign In
-                </Nav.Link>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
