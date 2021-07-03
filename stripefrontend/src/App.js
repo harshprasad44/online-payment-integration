0cdb945cc4071fd39f820f63586026d10360f315
Harsh Prasad <explorerharsh@gmail.com>
2021-07-03 12:59:20 +0530

    Seperating HomeScreen from App.js

--- stripefrontend/src/App.js
+++ stripefrontend/src/App.js
@@ -1,60 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import logo from "./logo.svg";
-import "./App.css";
-import StripeCheckout from "react-stripe-checkout";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 
-function App({ history }) {
-  const [product, setProduct] = useState({
-    name: "React from FB",
-    price: 10,
-    productBy: "facebook",
-  });
-
-  const [success, setSucceess] = useState(false);
-
-  // useEffect(() => {
-  //   if (success) {
-  //     <Redirect to="/success" />;
-  //   }
-  // }, [success]);
-
-  const makePayment = (token) => {
-    const body = {
-      token,
-      product,
-    };
-    const headers = {
-      "Content-Type": "application/json",
-    };
-
-    return fetch(`http://localhost:8282/payment`, {
-      method: "POST",
-      headers,
-      body: JSON.stringify(body),
-    })
-      .then((response) => {
-        console.log("RESPONSE ", response);
-        const { status } = response;
-        console.log("STATUS ", status);
-        setSucceess(true);
-      })
-      .catch((error) => console.log(error));
-  };
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <StripeCheckout stripeKey="pk_test_51J8hq2SFKDXtErfjmVAfvwIv4esMWrTQQCztHec2mwn6dhWdSLhq2K9wMap5zaRZtEncEELHUYeiNn7wa0lRdqZq00mGaZt4BE" token={makePayment} name="Buy React" amount={product.price * 100} shippingAddress billingAddress>
-          <button className="btn-large green">Buy Now</button>
-        </StripeCheckout>
-
-        {success && <a href="/success">Success</a>}
-      </header>
-    </div>
+    <Router>
+      {/* <Header /> */}
+      <main className="py-3" style={{ backgroundColor: "#F5F0F9", borderRadius: "24px" }}>
+        {/* <Container> */}
+        {/* <Route path="/order/:id" component={OrderScreen} /> */}
+        <Route path="/" component={HomeScreen} exact />
+        {/* </Container> */}
+      </main>
+      {/* <Footer /> */}
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ stripefrontend/src/screens/HomeScreen.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+function HomeScreen() {
+  const [product, setProduct] = useState({
+    name: "React from FB",
+    price: 10,
+    productBy: "facebook",
+  });
+
+  const [success, setSucceess] = useState(false);
+
+  // useEffect(() => {
+  //   if (success) {
+  //     <Redirect to="/success" />;
+  //   }
+  // }, [success]);
+
+  const makePayment = (token) => {
+    const body = {
+      token,
+      product,
+    };
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    return fetch(`http://localhost:8282/payment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        console.log("RESPONSE ", response);
+        const { status } = response;
+        console.log("STATUS ", status);
+        setSucceess(true);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <StripeCheckout stripeKey="pk_test_51J8hq2SFKDXtErfjmVAfvwIv4esMWrTQQCztHec2mwn6dhWdSLhq2K9wMap5zaRZtEncEELHUYeiNn7wa0lRdqZq00mGaZt4BE" token={makePayment} name="Buy React" amount={product.price * 100} shippingAddress billingAddress>
+          <button className="btn-large green">Buy Now</button>
+        </StripeCheckout>
+
+        {success && <a href="/success">Success</a>}
+      </header>
+    </div>
+  );
+}
+
+export default HomeScreen;
