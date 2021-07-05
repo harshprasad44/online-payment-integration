08d86adc7301d971432129f40bc2792473c4267b
Harsh Prasad <explorerharsh@gmail.com>
2021-07-05 23:50:16 +0530

    Details Form Edit

--- stripefrontend/src/screens/HomeScreen.js
+++ stripefrontend/src/screens/HomeScreen.js
@@ -46,13 +46,13 @@ function HomeScreen({ history }) {
         <Form>
           <Form.Row>
             <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter name" />
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
             </Form.Group>
           </Form.Row>
 
@@ -70,14 +70,6 @@ function HomeScreen({ history }) {
             <Form.Group as={Col} controlId="formGridCity">
               <Form.Label>City</Form.Label>
               <Form.Control />
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="formGridState">
-              <Form.Label>State</Form.Label>
-              <Form.Control as="select" defaultValue="Choose...">
-                <option>Choose...</option>
-                <option>...</option>
-              </Form.Control>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridZip">
