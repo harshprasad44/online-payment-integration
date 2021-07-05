317de49b4df88a50a3ba1d9aefc6e6a77e2fd82b
Harsh Prasad <explorerharsh@gmail.com>
2021-07-05 23:39:45 +0530

    Adding details form

--- stripefrontend/src/screens/HomeScreen.js
+++ stripefrontend/src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import StripeCheckout from "react-stripe-checkout";
+import { Form, Col } from "react-bootstrap";
 
 function HomeScreen({ history }) {
   const [product, setProduct] = useState({
@@ -42,6 +43,49 @@ function HomeScreen({ history }) {
   return (
     <div className="App">
       <header className="App-header">
+        <Form>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Group controlId="formGridAddress1">
+            <Form.Label>Address</Form.Label>
+            <Form.Control placeholder="1234 Main St" />
+          </Form.Group>
+
+          <Form.Group controlId="formGridAddress2">
+            <Form.Label>Address 2</Form.Label>
+            <Form.Control placeholder="Apartment, studio, or floor" />
+          </Form.Group>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Label>City</Form.Label>
+              <Form.Control />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridState">
+              <Form.Label>State</Form.Label>
+              <Form.Control as="select" defaultValue="Choose...">
+                <option>Choose...</option>
+                <option>...</option>
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridZip">
+              <Form.Label>Zip</Form.Label>
+              <Form.Control />
+            </Form.Group>
+          </Form.Row>
+        </Form>
         <StripeCheckout stripeKey="pk_test_51J8hq2SFKDXtErfjmVAfvwIv4esMWrTQQCztHec2mwn6dhWdSLhq2K9wMap5zaRZtEncEELHUYeiNn7wa0lRdqZq00mGaZt4BE" token={makePayment} name="Online Payment Integration" amount={product.price * 100} shippingAddress billingAddress>
           <button className="btn-large green">Buy Now</button>
         </StripeCheckout>
