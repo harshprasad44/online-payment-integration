f367f5835bc228fb12fe625e3d939f3348a2a069
Harsh Prasad <explorerharsh@gmail.com>
2021-07-06 12:59:24 +0530

    Redux configuration completed

--- stripefrontend/src/screens/HomeScreen.js
+++ stripefrontend/src/screens/HomeScreen.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Form, Col, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
 
 const HomeScreen = ({ history }) => {
   const submitHandler = (e) => {
@@ -25,23 +24,23 @@ const HomeScreen = ({ history }) => {
 
         <Form.Group controlId="formGridAddress1">
           <Form.Label>Address</Form.Label>
-          <Form.Control required placeholder="1234 Main St" />
+          <Form.Control type="text" required placeholder="1234 Main St" />
         </Form.Group>
 
         <Form.Group controlId="formGridAddress2">
           <Form.Label>Address 2</Form.Label>
-          <Form.Control placeholder="Apartment, studio, or floor" />
+          <Form.Control type="text" placeholder="Apartment, studio, or floor" />
         </Form.Group>
 
         <Form.Row>
           <Form.Group as={Col} controlId="formGridCity">
             <Form.Label>City</Form.Label>
-            <Form.Control required />
+            <Form.Control type="text" required />
           </Form.Group>
 
           <Form.Group as={Col} controlId="formGridZip">
             <Form.Label>Zip</Form.Label>
-            <Form.Control required />
+            <Form.Control type="text" required />
           </Form.Group>
         </Form.Row>
 
